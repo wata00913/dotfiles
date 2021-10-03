b0a4beb978fc10bde62282d14e538192ca6d3775
wata00913 <175d8639@gmail.com>
2021-10-03 20:07:15 +0900

    add cd history change function(hd)

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,7 +1,11 @@
+HIS_PATH="$HOME/.cache/shell/chpwd-recent-dirs"
+
 fd() {
     local select_dir=$(find . -path '*/.*' -prune -o -type d -print 2> /dev/null | fzf)
     cd $select_dir
 }
 
 hd() {
+    local select_his_dir=$(cat $HIS_PATH | sed -e "s/[$\']//g" | fzf)
+    cd $select_his_dir
 }
