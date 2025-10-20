b7fae41df8151d34fc5f76240ccb16c9eacdc9c8
wata00913 <175d8639@gmail.com>
2025-10-21 00:52:52 +0900

    lspを変更

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,246 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+    },
+    config = function()
+      local lspconfig = require('lspconfig')
+      local cmp_nvim_lsp = require('cmp_nvim_lsp')
+      
+      -- LSPのキーマップ設定
+      local on_attach = function(client, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        
+        vim.keymap.set('n', '<space>l[', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', '<space>l]', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<space>lr', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<space>la', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<space>ld', vim.lsp.buf.document_symbol, opts)
+        vim.keymap.set('n', '<space>lh', vim.lsp.buf.document_highlight, opts)
+        vim.keymap.set('n', '<space>lf', function()
+          vim.lsp.buf.format({ async = true })
+        end, opts)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+        vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '<space>le', vim.diagnostic.setloclist, opts)
+      end
+      
+      -- 診断表示の設定
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = '●',
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = 'rounded',
+          source = 'always',
+        },
+      })
+      
+      -- 診断記号の設定
+      local signs = { Error = '✖', Warn = '⚠', Hint = '', Info = '' }
+      for type, icon in pairs(signs) do
+        local hl = 'DiagnosticSign' .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+      
+      -- LSPの機能を補完に統合
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+      
+      -- デフォルトの設定
+      local default_setup = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
+      
+      -- 各言語サーバーの設定
+      -- TypeScript/JavaScript
+      lspconfig.ts_ls.setup(default_setup)
+      
+      -- Python
+      lspconfig.pyright.setup(default_setup)
+      
+      -- Ruby
+      lspconfig.solargraph.setup(default_setup)
+      
+      -- Lua
+      lspconfig.lua_ls.setup(vim.tbl_extend('force', default_setup, {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file('', true),
+              checkThirdParty = false,
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      }))
+      
+      -- PHP
+      lspconfig.intelephense.setup(default_setup)
+      
+      -- Go
+      lspconfig.gopls.setup(default_setup)
+      
+      -- Rust
+      lspconfig.rust_analyzer.setup(default_setup)
+      
+      -- HTML/CSS/JSON
+      lspconfig.html.setup(default_setup)
+      lspconfig.cssls.setup(default_setup)
+      lspconfig.jsonls.setup(default_setup)
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    cmd = 'Mason',
+    keys = { { '<leader>m', '<cmd>Mason<cr>', desc = 'Mason' } },
+    build = ':MasonUpdate',
+    config = function()
+      require('mason').setup({
+        ui = {
+          border = 'rounded',
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
+          },
+        },
+      })
+    end,
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = { 'williamboman/mason.nvim' },
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'ts_ls',
+          'pyright',
+          'solargraph',
+          'lua_ls',
+          'intelephense',
+          'gopls',
+          'rust_analyzer',
+          'html',
+          'cssls',
+          'jsonls',
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = '[LSP]',
+              luasnip = '[Snippet]',
+              buffer = '[Buffer]',
+              path = '[Path]',
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+      })
+      
+      -- コマンドライン補完
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+          { name = 'cmdline' },
+        }),
+      })
+      
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      })
+    end,
+  },
+  {
+    'L3MON4D3/LuaSnip',
+    version = 'v2.*',
+    build = 'make install_jsregexp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  },
+  {
+    'rafamadriz/friendly-snippets',
+    lazy = true,
+  },
+}
--- .config/nvim/lua/plugins/search.lua
+++ .config/nvim/lua/plugins/search.lua
@@ -3,23 +3,23 @@ return {
     'haya14busa/vim-migemo',
     event = 'VeryLazy',
   },
-  {
-    'haya14busa/incsearch.vim',
-    keys = { '/', '?', 'g/' },
-    config = function()
-      vim.keymap.set('', '/', '<Plug>(incsearch-forward)')
-      vim.keymap.set('', '?', '<Plug>(incsearch-backward)')
-      vim.keymap.set('', 'g/', '<Plug>(incsearch-stay)')
-    end,
-  },
-  {
-    'haya14busa/incsearch-migemo.vim',
-    dependencies = { 'haya14busa/incsearch.vim' },
-    keys = { 'm/', 'm?', 'mg/' },
-    config = function()
-      vim.keymap.set('', 'm/', '<Plug>(incsearch-migemo-/)')
-      vim.keymap.set('', 'm?', '<Plug>(incsearch-migemo-?)')
-      vim.keymap.set('', 'mg/', '<Plug>(incsearch-migemo-stay)')
-    end,
-  },
+  -- {
+  --   'haya14busa/incsearch.vim',
+  --   keys = { '/', '?', 'g/' },
+  --   config = function()
+  --     vim.keymap.set('', '/', '<Plug>(incsearch-forward)')
+  --     vim.keymap.set('', '?', '<Plug>(incsearch-backward)')
+  --     vim.keymap.set('', 'g/', '<Plug>(incsearch-stay)')
+  --   end,
+  -- },
+  -- {
+  --   'haya14busa/incsearch-migemo.vim',
+  --   dependencies = { 'haya14busa/incsearch.vim' },
+  --   keys = { 'm/', 'm?', 'mg/' },
+  --   config = function()
+  --     vim.keymap.set('', 'm/', '<Plug>(incsearch-migemo-/)')
+  --     vim.keymap.set('', 'm?', '<Plug>(incsearch-migemo-?)')
+  --     vim.keymap.set('', 'mg/', '<Plug>(incsearch-migemo-stay)')
+  --   end,
+  -- },
 }
